4a4ea3f9f662a8b57c40dc009415e5c578199c27
Luis Pabón <luis.pabon@hammerspace.com>
2025-10-27 14:49:21 -0400

    Update 10-27-2025

--- hack/docker-build.sh
+++ hack/docker-build.sh
@@ -25,17 +25,17 @@ fi
 ## VERSIONS
 ## Confirm that the links are correct. Some tools change the links on newer versions
 # https://go.dev/dl/
-GFGOLANG=1.24.4
+GFGOLANG=1.25.3
 # https://github.com/grpc-ecosystem/grpc-gateway/releases
-GFGRPCGATEWAY=2.27.1
+GFGRPCGATEWAY=2.27.3
 # https://github.com/pseudomuto/protoc-gen-doc/releases
 GFPROTOCGENDOC=1.5.1
 # https://github.com/protocolbuffers/protobuf/releases
-GFPROTOC=31.1
+GFPROTOC=33.0
 
 # Get gRPC golang versions from here: https://grpc.io/docs/languages/go/quickstart/
 # Also see: https://pkg.go.dev/google.golang.org/protobuf/cmd/protoc-gen-go
-GFPROTOCGENGO=1.36.6
+GFPROTOCGENGO=1.36.10
 # Also see: https://pkg.go.dev/google.golang.org/grpc/cmd/protoc-gen-go-grpc
 GFPROTOCGENGOGRPC=1.5.1
 
